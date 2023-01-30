8f3dbe62f7b0cbc09c0b30c2471d325f4ba6721f
Alysson Santos <alyssonassissantos@gmail.com>
2023-01-30 09:35:18 -0300

    Sistema de Bancom

---
+++ SistemaBanco/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace SistemaBanco {
+    internal class Conta {
+        public string Nome { get; set; }
+        public string SNome { get; set; }
+        public double Money { get; set; }
+
+
+        public Conta(string nome, string snome) {
+            Nome = nome;
+            SNome= snome; 
+        }
+        public Conta(string nome, string snome, double depositoInicial) : this (nome, snome) {
+            Deposito(depositoInicial);
+            
+        }
+
+        public void Deposito(double money) {
+           Money += money;
+        }
+        public void Saque (double money) {
+            Money -= money + 5.0;
+        }
+
+        public override string ToString() {
+            return "Nome: "+ Nome
+                +"\nSobrenome: "+ SNome
+                +"\nSaldo da conta: "
+                +Money.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ SistemaBanco/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaBanco {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Vou precisar dos seu dados pessoais {Nome, Sobrenome, Data de Nascimento, CPF, RG, Tell ou Cell}");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Sobrenome: ");
+            string snome = Console.ReadLine();
+           /* Console.Write("Data de Nascimento: ");
+            int dn = int.Parse(Console.ReadLine());
+            Console.Write("CPF: ");
+            long cpf = long.Parse(Console.ReadLine());
+            Console.Write("RG: ");
+            int rg = int.Parse(Console.ReadLine());
+            Console.Write("Tell: ");
+            int tell = int.Parse(Console.ReadLine());
+            Console.Write("Cell: ");
+            int cell = int.Parse(Console.ReadLine());
+            Console.WriteLine(); */
+
+            Conta cont;
+
+            Console.WriteLine("Deseja efetuar algum saque? S/N");
+            string resp = Console.ReadLine();
+            if (resp == "N" || resp == "n") {
+                cont = new Conta(nome, snome);
+                Console.WriteLine(cont);
+            }
+            else {
+                Console.Write("Qual o valor de Deposito? :");
+                double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                cont = new Conta(nome, snome,depositoInicial);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(cont);
+
+            Console.WriteLine();
+            Console.WriteLine("Efetuar um Deposito: ");
+            double quant = double.Parse(Console.ReadLine());
+            cont.Deposito(quant);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(cont);
+        }
+    }
+}
+
